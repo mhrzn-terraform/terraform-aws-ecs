6ba107719469c2ee1c9997b9fd1a7c19588cfcce
mhrznamn068 <mhrznamn068@gmail.com>
2024-09-09 21:05:10 +0545

    remove extra dns records

--- examples/full-demo/cluster.tf
+++ examples/full-demo/cluster.tf
@@ -1,6 +1,6 @@
 module "ecs_cluster" {
   source            = "cn-terraform/ecs-cluster/aws"
-  version           = "1.0.10"
+  version           = "1.0.11"
   name              = "${var.project_name}-${var.env}"
   containerInsights = var.containerInsights
 }
